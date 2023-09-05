cf167a820704c1420a1c14d04d4747d11648cc08
Leonides Muguercia <leonides.muguercia@neoris.com>
2023-09-05 09:55:30 +0200

    Create routes

---
+++ src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './dinamic-page.component.html',
+  styles: [
+  ]
+})
+export class DinamicPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
